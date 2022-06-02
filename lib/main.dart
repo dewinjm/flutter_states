9a1e285d1bef1b9813e7aa2551c5452d36aeb1e5
dewinjm <dewin.martinez@gmail.com>
2022-06-02 10:24:59 -0400

    Modify main: change title to Shop example

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shop',
+      title: 'Shop example',
       theme: ThemeData(
         primarySwatch: Palette.primaryMaterialColor,
       ),
