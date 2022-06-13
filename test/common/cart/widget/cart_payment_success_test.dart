1678a3ea345c148afc07be94de770dbeba862d12
dewinjm <dewin.martinez@gmail.com>
2022-06-13 08:52:50 -0400

    test: add cart widgets test

--- test/common/cart/widget/cart_payment_success_test.dart
+++ test/common/cart/widget/cart_payment_success_test.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:state_management/common/common.dart';
 
 import '../../../helpers/helpers.dart';
 
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+class FakeRoute extends Fake implements Route {}
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() {
+    registerFallbackValue(FakeRoute());
+  });
+
   group('CartPaymentSuccess', () {
     testWidgets('should renders', (tester) async {
       await tester.pumpApp(const CartPaymentSuccess());
@@ -12,5 +23,21 @@ void main() {
       expect(find.byType(AlertDialog), findsOneWidget);
       expect(find.text('Payment success'), findsOneWidget);
     });
+  });
+
+  testWidgets('should navigate back when pressed ElevatedButton',
+      (tester) async {
+    final mockObserver = MockNavigatorObserver();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: const CartPaymentSuccess(),
+        navigatorObservers: [mockObserver],
+      ),
+    );
+    expect(find.byType(IconButton), findsOneWidget);
+    await tester.tap(find.byType(IconButton));
+
+    verify(() => mockObserver.didPop(any(), any())).called(1);
   });
 }
---
+++ test/features/no_package/cart/state/cart_state_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:state_management/common/common.dart';
+import 'package:state_management/features/no_package/cart/cart.dart';
+
+class _MockCartRepository extends Mock implements CartRepository {}
+
+void main() {
+  late _MockCartRepository cartRepository;
+
+  setUp(() {
+    cartRepository = _MockCartRepository();
+  });
+
+  group('CartState InheritedWidget', () {
+    const fakeCart = Cart(
+      item: Catalog(
+          id: 1,
+          name: 'Fake Name',
+          unit: '1lb',
+          price: 5,
+          imageAsset: 'fake_path'),
+      count: 10,
+    );
+
+    testWidgets(
+      'Update inherited when change state',
+      (WidgetTester tester) async {
+        late CartState inner;
+
+        final Widget widget = CartState(
+          cartRepository: cartRepository,
+          cartNotifier: CartNotifier([fakeCart]),
+          child: Builder(
+            builder: (BuildContext context) {
+              inner = context.dependOnInheritedWidgetOfExactType<CartState>()!;
+              return Container();
+            },
+          ),
+        );
+
+        await tester.pumpWidget(widget);
+
+        when(
+          () => cartRepository.send(
+            cartItems: inner.cartNotifier.items,
+          ),
+        ).thenAnswer((_) async => true);
+
+        await inner.process();
+
+        expect(inner.cartNotifier.items.length, equals(0));
+        expect(inner.updateShouldNotify(inner), false);
+      },
+    );
+  });
+}
